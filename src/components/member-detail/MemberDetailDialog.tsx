525f48fd54e20087f13b4a7b4853d2bd7f0a3caa
Maximilian Smidt <134879273+maxi-smidt@users.noreply.github.com>
2026-02-15 18:02:50 +0100

    [Feature #71] detail member sheet width (#72)

--- src/components/member-detail/MemberDetailDialog.tsx
+++ src/components/member-detail/MemberDetailDialog.tsx
@@ -85,7 +85,7 @@ export const MemberDetailDialog = ({ member, open, onOpenChange }: Props) => {
   return (
     <>
       <Dialog open={open} onOpenChange={onOpenChange}>
-        <DialogContent className="max-w-[95vw] w-full max-h-[95vh] h-full overflow-y-auto p-8">
+        <DialogContent className="sm:max-w-[90%] w-full max-h-[95vh] h-full overflow-y-auto p-8">
           <DialogHeader>
             <DialogTitle className="text-3xl font-bold">
               {member.firstName} {member.lastName}
